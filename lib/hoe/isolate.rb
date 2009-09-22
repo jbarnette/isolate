500a60d62c360c72ebb25542110b0ec87fd8b812
John Barnette <jbarnette@gmail.com>
2009-09-22 00:43:56 -0700

    Install is default now, squish warning.

--- lib/hoe/isolate.rb
+++ lib/hoe/isolate.rb
@@ -5,13 +5,13 @@ module Hoe::Isolate
   attr_accessor :isolate_dir
 
   def initialize_isolate
-    # Tee hee! Move ourselves to the front to beat out :test
-    Hoe.plugins.unshift Hoe.plugins.delete :isolate
+    # Tee hee! Move ourselves to the front to beat out :test.
+    Hoe.plugins.unshift Hoe.plugins.delete(:isolate)
     self.isolate_dir ||= "tmp/gems"
   end
 
   def define_isolate_tasks
-    i = Isolate.new self.isolate_dir, :install => true, :verbose => true
+    i = Isolate.new self.isolate_dir
 
     (self.extra_deps + self.extra_dev_deps).each do |name, version|
       i.gem name, *Array(version)
