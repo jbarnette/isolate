5dfb9ea88e1f0db0941f2bfdbacddc2a781d3eae
Ryan Davis <ryand-ruby@zenspider.com>
2009-09-22 00:36:03 -0700

    ! Added hoe plugin, for justice!

--- Rakefile
+++ Rakefile
@@ -5,9 +5,12 @@ Hoe.plugin :doofus, :git
 
 Hoe.spec "isolate" do
   developer "John Barnette", "jbarnette@rubyforge.org"
+  developer "Ryan Davis",    "ryand-ruby@zenspider.com"
 
   self.extra_rdoc_files = Dir["*.rdoc"]
   self.history_file     = "CHANGELOG.rdoc"
   self.readme_file      = "README.rdoc"
   self.testlib          = :minitest
+
+  extra_dev_deps << ["minitest", "~> 1.4"]
 end
---
+++ lib/hoe/isolate.rb
@@ -0,0 +1,22 @@
+require "rubygems"
+require "isolate"
+
+module Hoe::Isolate
+  attr_accessor :isolate_dir
+
+  def initialize_isolate
+    # Tee hee! Move ourselves to the front to beat out :test
+    Hoe.plugins.unshift Hoe.plugins.delete :isolate
+    self.isolate_dir ||= "tmp/gems"
+  end
+
+  def define_isolate_tasks
+    i = Isolate.new self.isolate_dir, :install => true, :verbose => true
+
+    (self.extra_deps + self.extra_dev_deps).each do |name, version|
+      i.gem name, *Array(version)
+    end
+
+    i.activate
+  end
+end
