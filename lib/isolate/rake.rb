ce49b712e42ac1cf97528e16fc4e918e9bb69bba
John Barnette <jbarnette@gmail.com>
2009-10-03 12:53:59 -0700

    Add Rake helpers.

---
+++ lib/isolate/rake.rb
@@ -0,0 +1,25 @@
+namespace :isolate do
+  desc "Generate a .gems manifest for your isolated gems."
+  task :dotgems do
+    File.open ".gems", "wb" do |f|
+      Isolate.instance.entries.each do |entry|
+        next unless entry.environments.empty?
+
+        gems  = [entry.name]
+        gems << "--version '#{entry.requirement}'"
+        gems << "--source #{entry.options[:source]}" if entry.options[:source]
+
+        f.puts gems.join " "
+      end
+
+      # this above all: to thine own self be true
+      f.puts "isolate --version '#{Isolate::VERSION}'"
+    end
+  end
+
+  desc "Start an isolated subshell. Run 'command' and exit if specified."
+  task :sh, [:command] do |t, args|
+    exec ENV["SHELL"] unless args.command
+    sh args.command
+  end
+end
