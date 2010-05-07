490b178f13574a13b9f9aa1ff7249210b3a0cf97
John Barnette <jbarnette@gmail.com>
2010-05-07 10:39:56 -0700

    Prep for v2.0.0.pre.2.

--- lib/isolate.rb
+++ lib/isolate.rb
@@ -8,7 +8,7 @@ module Isolate
 
   # Duh.
 
-  VERSION = "2.0.0.pre.1"
+  VERSION = "2.0.0.pre.2"
 
   # Disable Isolate. If a block is provided, isolation will be
   # disabled for the scope of the block.
